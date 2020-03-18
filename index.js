475eb47a0062f79dd6eb8dfcecce39ff6c558daf
James Stamps Jr <jstampshere@gmail.com>
2020-03-18 17:25:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
   
-  return animal
+  
 }
+return animal
+
 
 function add2(n) {
   return n + two
